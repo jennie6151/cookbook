608e285d9f50a4ba7ee4dbf850d86ada48ee6be8
Jennie <36511194+jennie6151@users.noreply.github.com>
2019-05-23 14:32:07 +0100

    viewRecipes page added. placeholders used.

--- flask_app/app.py
+++ flask_app/app.py
@@ -10,5 +10,9 @@ def home():
 def viewRecipes():
     return render_template("viewrecipes.html")
 
+@app.route("/addRecipes")
+def addRecipes():
+    return render_template("addrecipes.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
