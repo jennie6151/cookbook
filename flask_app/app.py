ce84f9c6d71978c43e5ba4c93c082ea5c97216a6
Jennie <36511194+jennie6151@users.noreply.github.com>
2019-05-24 15:00:29 +0100

    Correctly writing to the database

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 from bson import ObjectId
 from pymongo import MongoClient
 import os
@@ -6,23 +6,40 @@ import os
 app = Flask(__name__)
 
 
-client = MongoClient("mongodb+srv://app1:fRH3SQCA9Tq5VG18@myfirstcluster-7pmlf.mongodb.net/Cookbook?retryWrites=true")
+client = MongoClient(
+    "mongodb+srv://app1:fRH3SQCA9Tq5VG18@myfirstcluster-7pmlf.mongodb.net/Cookbook?retryWrites=true")
 db = client.Cookbook
 Recipes = db.Recipes
+
 
 @app.route("/")
 def home():
     pageRecipes = Recipes.find()
-    return render_template("index.html",pageRecipes=pageRecipes)
+    return render_template("index.html", pageRecipes=pageRecipes)
 
 @app.route("/viewRecipes")
 def viewRecipes():
     return render_template("viewrecipes.html")
 
+
 @app.route("/addRecipes")
 def addRecipes():
     return render_template("addrecipes.html")
 
+
+@app.route("/action", methods=['POST'])
+def action():
+    username = request.values.get("username")
+    recipeTitle = request.values.get("recipeTitle")
+    recipeIngredients = request.values.get("recipeIngredients")
+    recipeMethod = request.values.get("recipeMethod")
+    prepTime = request.values.get("prepTime")
+    cookTime = request.values.get("cookTime")
+    recipeServes = request.values.get("recipeServes")
+    recipeNotes = request.values.get("recipeNotes")
+    Recipes.insert({"username": username, "name": recipeTitle, "recipeIngredients": recipeIngredients,
+                    "recipeMethod": recipeMethod, "prepTime": prepTime, "cookTime": cookTime, "recipeServes": recipeServes, "recipeNotes": recipeNotes})
+    return redirect("/")
+
 if __name__ == "__main__":
     app.run(debug=True)
-
