a3ef44797d966b60d6e15607ebb8ad144feacfef
Jennie <36511194+jennie6151@users.noreply.github.com>
2019-06-06 11:43:15 +0100

    like function added

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from bson import ObjectId
 from flask_pymongo import PyMongo
-import os, random
+import os
+import random
 
 app = Flask(__name__)
 
@@ -10,30 +11,36 @@ mongo = PyMongo(app)
 
 Recipes = mongo.db.Recipes
 
+
 @app.route("/")
 def home():
     pageRecipes = Recipes.find().sort("_id", -1).limit(12)
     return render_template("index.html", pageRecipes=pageRecipes)
 
-@app.route("/search", methods=['GET'])  
+
+@app.route("/search", methods=['GET'])
 def search():
-    recipeSearch=request.values.get("recipeSearch")  
-    refer="recipeTitle"
-    if(recipeSearch=="_id"):
-        recipe = Recipes.find({refer:ObjectId(recipeSearch)})  
+    recipeSearch = request.values.get("recipeSearch")
+    refer = "recipeTitle"
+    if(recipeSearch == "_id"):
+        recipe = Recipes.find({refer: ObjectId(recipeSearch)})
     else:
-        results = Recipes.find({'recipeTitle':{'$regex':'/.*'+recipeSearch+'.*/'}})
-    return render_template('searchresults.html',searchResults=results)  
+        results = Recipes.find(
+            {'recipeTitle': {'$regex': '/.*'+recipeSearch+'.*/'}})
+    return render_template('searchresults.html', searchResults=results)
+
 
 @app.route("/viewRecipes")
 def viewRecipes():
-    id=request.values.get("_id")  
-    recipe=Recipes.find({"_id":ObjectId(id)}) 
+    id = request.values.get("_id")
+    recipe = Recipes.find({"_id": ObjectId(id)})
     return render_template("viewrecipes.html", recipe=recipe)
+
 
 @app.route("/addRecipes")
 def addRecipes():
     return render_template("addrecipes.html")
+
 
 @app.route("/createRecipe", methods=['POST'])
 def action():
@@ -41,7 +48,7 @@ def action():
     recipeTitle = request.values.get("recipeTitle")
     if "recipeImage" in request.files:
         recipeImage = request.files["recipeImage"]
-        randomNumber= int(random.random() * 10000)
+        randomNumber = int(random.random() * 10000)
         mongo.save_file(str(randomNumber) + recipeImage.filename, recipeImage)
     recipeIngredients = request.values.get("recipeIngredients")
     recipeMethod = request.values.get("recipeMethod")
@@ -56,24 +63,27 @@ def action():
                     "recipeMethod": recipeMethod, "prepTime": prepTime, "cookTime": cookTime, "recipeDifficulty": recipeDifficulty, "recipeServes": recipeServes, "dietType": dietType, "cuisineType": cuisineType, "recipeNotes": recipeNotes})
     return redirect("/")
 
-@app.route("/edit")  
-def edit ():  
-    id=request.values.get("_id")  
-    recipe=Recipes.find({"_id":ObjectId(id)})  
+
+@app.route("/edit")
+def edit():
+    id = request.values.get("_id")
+    recipe = Recipes.find({"_id": ObjectId(id)})
     return render_template('editrecipes.html', recipe=recipe)
 
-@app.route("/updateRecipe", methods=['POST'])  
-def updateRecipe ():
-    id=request.values.get("_id") 
+
+@app.route("/updateRecipe", methods=['POST'])
+def updateRecipe():
+    id = request.values.get("_id")
     username = request.values.get("username")
     recipeTitle = request.values.get("recipeTitle")
     if "recipeImage" in request.files:
         recipeImage = request.files["recipeImage"]
         if recipeImage.filename != "":
-            randomNumber= int(random.random() * 10000)
+            randomNumber = int(random.random() * 10000)
             newFileName = str(randomNumber) + recipeImage.filename
             mongo.save_file(newFileName, recipeImage)
-            Recipes.update({"_id":ObjectId(id)}, {'$set': {"recipeImage":newFileName}})
+            Recipes.update({"_id": ObjectId(id)}, {
+                           '$set': {"recipeImage": newFileName}})
 
     recipeIngredients = request.values.get("recipeIngredients")
     recipeMethod = request.values.get("recipeMethod")
@@ -84,19 +94,36 @@ def updateRecipe ():
     dietType = request.form.getlist("dietType")
     cuisineType = request.form.get("cuisineType")
     recipeNotes = request.values.get("recipeNotes")
-    Recipes.update({"_id":ObjectId(id)}, {'$set':{"username":username, "recipeTitle":recipeTitle, "recipeIngredients":recipeIngredients,
-                    "recipeMethod":recipeMethod, "prepTime":prepTime, "cookTime":cookTime, "recipeDifficulty": recipeDifficulty, "recipeServes": recipeServes, "dietType": dietType, "cuisineType": cuisineType, "recipeNotes":recipeNotes}})
+    Recipes.update({"_id": ObjectId(id)}, {'$set': {"username": username, "recipeTitle": recipeTitle, "recipeIngredients": recipeIngredients,
+                                                    "recipeMethod": recipeMethod, "prepTime": prepTime, "cookTime": cookTime, "recipeDifficulty": recipeDifficulty, "recipeServes": recipeServes, "dietType": dietType, "cuisineType": cuisineType, "recipeNotes": recipeNotes}})
     return redirect("/")
 
-@app.route("/remove")  
-def remove ():  
-    id=request.values.get("_id")  
-    Recipes.remove({"_id":ObjectId(id)})  
+
+@app.route("/like", methods=['POST'])
+def like():
+    id = request.values.get("_id")
+    like = 0
+    databaseResults = Recipes.find_one({"_id": ObjectId(id)})
+    try:
+        like = int(databaseResults["like"])
+    except:
+        print("not found")
+    like = int(like)+1
+    Recipes.update({"_id": ObjectId(id)}, {'$set': {"like": like}})
+    return redirect(url_for('viewRecipes', _id=id))
+
+
+@app.route("/remove")
+def remove():
+    id = request.values.get("_id")
+    Recipes.remove({"_id": ObjectId(id)})
     return redirect("/")
+
 
 @app.route('/file/<filename>')
 def file(filename):
     return mongo.send_file(filename)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
